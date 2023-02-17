a3f97cf9b22b14f96498597b7309674761a6dd98
WaiKeat <wklai368@gmail.com>
2023-02-17 23:29:58 +0800

    Created passenger database

---
+++ Database/passengerdb.sql
@@ -0,0 +1,19 @@
+DROP database if exists passenger_db;
+CREATE database passenger_db;
+USE passenger_db;
+
+# Creating Table
+CREATE TABLE Passengers (
+ PassengerID VARCHAR(10) PRIMARY KEY UNIQUE,
+ FirstName VARCHAR(50) NOT NULL,
+ LastName VARCHAR(50) NOT NULL,
+ PhoneNum varchar(8) NOT NULL unique,
+ Email VARCHAR(100) NOT NULL unique
+ );
+
+#===============================[Creating Rows for Passenger]===============================#
+INSERT INTO Passengers (PassengerID, FirstName, LastName, PhoneNum, Email) VALUES ("P1","Peter","Hung","93629930","peterhung@gmail.com");
+INSERT INTO Passengers (PassengerID, FirstName, LastName, PhoneNum, Email) VALUES ("P2","Charis","Tang","91345690","charistang@hotmail.com");
+INSERT INTO Passengers (PassengerID, FirstName, LastName, PhoneNum, Email) VALUES ("P3","Mike","Wee","98901234","mikewee@gmail.com");
+
+select * from passengers
